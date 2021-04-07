a6098f1d4fbfaf06dc01dd0d653ba3f1b9f4ddb1
rgerum <14153051+rgerum@users.noreply.github.com>
2021-04-07 17:56:34 -0400

    initial commit

---
+++ CORNetZ/datagenerator_v.py
@@ -0,0 +1,111 @@
+import numpy as np
+from tensorflow import keras
+import tensorflow as tf
+from PIL import Image
+from pathlib import Path
+
+
+class ImageDataGeneratorV(keras.utils.Sequence):
+    """Generates data for Keras"""
+
+    def __init__(self, txt_file, mode="training", batch_size=50, img_size=227, shuffle=True, buffer_size=1000, length=None):
+        self.txt_file = txt_file
+        files = {"V1": "V1_train.txt", "V4": "V4.txt", "IT": "IT.txt"}
+        if txt_file in files:
+            self.txt_file = Path("data/V1") / files[txt_file]
+
+        if not Path(self.txt_file).exists():
+            from urllib.request import urlopen
+            from io import BytesIO
+            import zipfile
+
+            print("downloading monkey v1 dataset...")
+            url = urlopen("https://github.com/rgerum/TrainCNNsWithNeuralData_keras/releases/download/v0.0/monkey_v1.zip")
+            data = url.read()
+            file = BytesIO(data)
+            Path("data/V1").mkdir(parents=True, exist_ok=True)
+            with zipfile.ZipFile(file) as zip_ref:
+                zip_ref.extractall("data/V1")
+
+        self.img_size = int(img_size)
+        self.shuffle = shuffle
+
+        # retrieve the data from the text file
+        self._read_txt_file()
+
+        # number of samples in the dataset
+        self.data_size = len(self.RSAs)
+        self.batch_size = batch_size
+
+        self.overhang = 0
+        self.random = np.random.default_rng()
+        if length is not None:
+            self.overhang = length * self.batch_size - self.batch_size
+            self.length = length
+        else:
+            self.length = int(np.floor(self.data_size / self.batch_size))
+
+        self.on_epoch_end()
+
+    def _read_txt_file(self):
+        """Read the content of the text file and store it into lists."""
+        self.img1_paths = []
+        self.img2_paths = []
+        self.RSAs = []
+        with open(self.txt_file, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                items = line.split()
+                self.img1_paths.append(Path("data/V1") / items[0])
+                self.img2_paths.append(Path("data/V1") / items[1])
+                self.RSAs.append(float(items[2]))
+
+    def __len__(self):
+        """Denotes the number of batches per epoch"""
+        return self.length
+
+    img1 = None
+    img2 = None
+    rsa = None
+    last_index = None
+    def __getitem__(self, index):
+        """Generate one batch of data"""
+        if self.last_index == index:
+            return [self.img1, self.img2], self.rsa
+
+        # Generate indexes of the batch
+        indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
+
+        # Find list of IDs
+        if self.img1 is None:
+            self.img1 = np.array([self.imread(self.img1_paths[k]) for k in indexes])
+            self.img2 = np.array([self.imread(self.img2_paths[k]) for k in indexes])
+            self.rsa = np.array([self.RSAs[k] for k in indexes], dtype=np.float32)[:, None]
+        else:
+            for i, k in enumerate(indexes):
+                self.img1[i] = self.imread(self.img1_paths[k])
+                self.img2[i] = self.imread(self.img2_paths[k])
+                self.rsa[i] = self.RSAs[k]
+        self.last_index = index
+
+        return [self.img1, self.img2], self.rsa
+
+    def on_epoch_end(self):
+        """Updates indexes after each epoch"""
+        self.indexes = np.concatenate((self.random.permutation(self.data_size), self.random.permutation(self.data_size)[:self.overhang]))
+
+    def imread(self, filename):
+        # load and preprocess the image
+        if 0:
+            img_resized = Image.open(filename).convert('RGB').crop((146, 60, 512, 426))
+            img_resized = np.array(img_resized.resize([self.img_size, self.img_size], Image.BILINEAR))
+            img_resized = img_resized.astype(np.float32)
+        #img_resized -= 128
+        #img_resized /= 128
+        else:
+            img_resized = keras.preprocessing.image.load_img(filename, color_mode="rgb",
+                                                             target_size=(int(self.img_size), int(self.img_size)),
+                                                             interpolation="bilinear")
+            img_resized = keras.preprocessing.image.img_to_array(img_resized)
+
+        return img_resized
---
+++ CORNetZ/run_cornetZ_ratio.py
@@ -0,0 +1,123 @@
+import argparse
+from datetime import datetime
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+from pathlib import Path
+
+from CORNetZ_V import CORNetZV, CosineDistance
+from datagenerator import ImageDataGenerator, MergedGenerators, TrainingHistory
+from datagenerator_v import ImageDataGeneratorV
+
+
+def run_net(args):
+    print(args)
+    np.random.seed(1234)
+
+    # data
+    tr_data = ImageDataGenerator("train.txt",
+                     img_size=args.img_size, batch_size=args.batch_size, num_classes=args.num_classes, shuffle=True)
+    val_data = ImageDataGenerator("val.txt",
+                     img_size=args.img_size, batch_size=args.batch_size, num_classes=args.num_classes, shuffle=False)
+
+    # the input image from the
+    img = tf.keras.layers.Input([args.img_size, args.img_size, 3], name="img")
+
+    # Initialize model
+    cornet = CORNetZV(args.num_classes, args.dropout_rate)
+
+    cls = cornet.forward(img)
+
+    model_class = keras.models.Model(inputs=img, outputs=cls)
+    opti = keras.optimizers.SGD(learning_rate=args.learning_rate)
+
+    def logit_cost(y, x):
+        return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=x))
+    model_class.compile(optimizer=opti, loss=logit_cost, metrics=["accuracy"])
+    print(model_class.summary())
+
+    # generate the output path
+    output_path = Path("logs") / (datetime.now().strftime("%Y%m%d-%H%M%S") + " " + " ".join([str(k) + "=" + str(v) for k, v in argp._get_kwargs()]))
+
+    # callbacks
+    calbacks = [
+        keras.callbacks.ModelCheckpoint(str(output_path / "weights.hdf5"), save_best_only=True, save_weights_only=True),
+        TrainingHistory(output_path)]
+
+    if args.v != 'None':
+        # get the data generator for the images from the neuronal data
+        v_data = ImageDataGeneratorV(args.v, batch_size=args.v_batch_size, length=len(tr_data))
+
+        # take two images as input
+        img1 = tf.keras.layers.Input([args.img_size, args.img_size, 3], name="img1")
+        img2 = tf.keras.layers.Input([args.img_size, args.img_size, 3], name="img2")
+
+        # process then with the V1 part of the model
+        img1_v1 = cornet.forward_V1(img1)
+        img2_v2 = cornet.forward_V1(img2)
+
+        # calculate the cosine distance
+        rsa = CosineDistance(name="rsa")([tf.keras.layers.Flatten()(img1_v1), tf.keras.layers.Flatten()(img2_v2)])
+        # multiply by weighting factor
+        lambd = tf.keras.layers.Input([1], name="lambd")
+        lambd_rsa = tf.keras.layers.Lambda(lambda x: x[0]*x[1], name="rsa_lambda")([rsa, lambd])
+
+        model_both = keras.models.Model(inputs=[img, img1, img2, lambd], outputs=[cls, lambd_rsa])
+        print(model_both.summary())
+        #tf.keras.utils.plot_model(model_both, to_file='model.png', show_shapes=True, expand_nested=True)
+
+        model_both.compile(optimizer=opti,
+                           loss={"class": logit_cost, "rsa_lambda": "mean_absolute_error"},
+                           metrics=["accuracy"])
+        print(model_both.summary())
+
+        model_rsa = keras.models.Model(inputs=[img1, img2], outputs=[rsa])
+        model_rsa.compile(optimizer="adam", loss={"rsa": "mean_absolute_error"}, metrics=["accuracy"])
+
+        class CustomCallback(keras.callbacks.Callback):
+            def __init__(self, data_gen):
+                self.data_gen = data_gen
+
+            def on_train_batch_begin(self, batch, logs=None):
+                if args.r_per_epoch is False:
+                    i, o = self.data_gen[batch]
+                    loss, class_loss, rsa_loss, class_acc, rsa_acc = model_both.evaluate(i, o, verbose=False)
+                    lambd = args.r * class_loss / rsa_loss
+                    self.data_gen.lambd = lambd
+
+            def on_epoch_begin(self, epoch, logs=None):
+                if args.r_per_epoch is True:
+                    import numpy as np
+                    i, o = self.data_gen[np.random.randint(len(self.data_gen))]
+                    loss, class_loss, rsa_loss, class_acc, rsa_acc = model_both.evaluate(i, o, verbose=False)
+                    lambd = args.r * class_loss / rsa_loss
+                    self.data_gen.lambd = lambd
+
+        training_input = MergedGenerators(tr_data, v_data)
+        training_input2 = MergedGenerators(val_data, v_data, use_min_length=True)
+
+        # the initial epochs with teacher
+        model_both.fit(training_input, validation_data=training_input2, shuffle=False, epochs=10, callbacks=[CustomCallback(training_input)]+calbacks)
+        # the rest of the epochs without a teacher
+        model_class.fit(tr_data, validation_data=val_data, epochs=args.num_epochs, callbacks=calbacks, initial_epoch=10)
+    else:
+        # train without teacher signal
+        model_class.fit(tr_data, validation_data=val_data, epochs=args.num_epochs, callbacks=calbacks)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Settings to run CORNet')
+    parser.add_argument('--v', default="V1", help='visual info', choices=['V1', 'V4', 'IT', 'None'])
+    parser.add_argument('--num_epochs', default=100, help='number of epochs')
+    parser.add_argument('--step', default=100, help='n steps to do v update')
+    parser.add_argument('--r', default=0.1, help='ratio of visual data to run')
+    parser.add_argument('--img_size', default=227, help='size to crop cif100 images')
+    parser.add_argument('--batch_size', default=128, help='the batch size')
+    parser.add_argument('--v_batch_size', default=128, help='the v batch size')
+    parser.add_argument('--dropout_rate', default=0.5, help='the drop out factor')
+    parser.add_argument('--num_classes', default=100, help='the drop out factor')
+    parser.add_argument('--learning_rate', default=0.001)
+    parser.add_argument('--r_per_epoch', default=False)
+    argp = parser.parse_args()
+
+    run_net(argp)
